79a58490ed424eb0949bb1e1acbe30ae277fb215
Andrés Gutiérrez <andres_mau.gutierrez@uao.edu.co>
2023-01-07 18:24:33 +0000

    Implementando endpoint para obtener ordenes dado JWT de usuario autenticado

--- routes/orders.router.js
+++ routes/orders.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 
 const OrderService = require('../services/order.service');
 const validatorHandler = require('../middlewares/validator.handler');
@@ -27,11 +28,13 @@ router.get(
 
 router.post(
   '/',
-  validatorHandler(createOrderSchema, 'body'),
+  passport.authenticate('jwt', {session: false}),
   async (req, res, next) => {
     try {
-      const body = req.body;
-      const newOrder = await service.create(body);
+      const userId = req.user.sub;
+      const newOrder = await service.create({
+        userId
+      });
       res.status(201).json(newOrder);
     } catch (error) {
       next(error);
